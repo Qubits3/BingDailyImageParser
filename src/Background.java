511f6f18214be9454b7ab8201d16ab9984fec669
Metin Yıldıran <metin.1464@gmail.com>
2020-03-10 12:59:47 +0300

    Added icon

--- src/Background.java
+++ src/Background.java
@@ -1,15 +1,19 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class Background {
 
-    public static void main (String [] args) {
+    public static void main (String [] args) throws IOException {
         if (!SystemTray.isSupported()) {
             System.out.println("SystemTray is not supported");
             return;
         }
-        Image image = Toolkit.getDefaultToolkit().getImage("MY/PATH/TO_IMAGE");
+        //Image image = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Metin\\Desktop\\icon.png");
+        Image image = ImageIO.read(Background.class.getResource("icon.jpg"));
 
         final PopupMenu popup = new PopupMenu();
         final TrayIcon trayIcon = new TrayIcon(image, "Wallpaper Changer", popup);
--- src/WallpaperChanger.java
+++ src/WallpaperChanger.java
@@ -23,7 +23,7 @@ public class WallpaperChanger {
         long SPIF_UPDATEINIFILE = 0x01;
         long SPIF_SENDWININICHANGE = 0x02;
 
-        SPI INSTANCE = (SPI) Native.loadLibrary("user32", SPI.class, new HashMap<String, Object>() {
+        SPI INSTANCE = Native.loadLibrary("user32", SPI.class, new HashMap<String, Object>() {
             {
                 put(OPTION_TYPE_MAPPER, W32APITypeMapper.UNICODE);
                 put(OPTION_FUNCTION_MAPPER, W32APIFunctionMapper.UNICODE);
