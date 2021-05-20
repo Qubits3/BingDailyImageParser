2513f840ccdaa863f2747cc6f6870269dcf0fa43
Metin Yıldıran <metin.1464@gmail.com>
2021-05-20 14:00:45 +0300

    Added image location textbox at the bottom of the screen.

--- src/Background.java
+++ src/Background.java
@@ -1,26 +1,31 @@
+import com.sun.deploy.panel.JSmartTextArea;
+
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.util.Date;
+import java.util.Objects;
 
 public class Background {
 
     public static void main(String[] args) throws IOException {
+
         if (!SystemTray.isSupported()) {
             System.out.println("SystemTray is not supported");
             return;
         }
         //Image image = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Metin\\Desktop\\icon.png");
-        Image image = ImageIO.read(Background.class.getResource("icon.png"));
+        Image image = ImageIO.read(Objects.requireNonNull(Background.class.getResource("icon.png")));
 
         final PopupMenu popup = new PopupMenu();
         final TrayIcon trayIcon = new TrayIcon(image, "Wallpaper Changer", popup);
         final SystemTray tray = SystemTray.getSystemTray();
 
         MenuItem exitItem = new MenuItem("   Exit   ");
-        exitItem.addActionListener(e -> System.exit(1));
+        exitItem.addActionListener(e -> System.exit(0));
         popup.add(exitItem);
 
         trayIcon.setPopupMenu(popup);
@@ -47,12 +52,47 @@ public class Background {
             }
 
             ImageParser.parseImage();
+
+            showTitle();
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        //Exit program when job finished
-        System.exit(1);
+        //Closes the program when its job finished
+//        System.exit(1);
     }
 
+    static void showTitle(){
+        JDialog jDialog = new JDialog();
+        jDialog.setUndecorated(true);
+        jDialog.setAlwaysOnTop(false);
+        jDialog.setOpacity(0.5f);
+        jDialog.getContentPane().setBackground(Color.BLACK);
+        jDialog.setType(Window.Type.UTILITY);   // Hide the program from alt + tab
+
+        JTextArea jSmartTextArea = new JTextArea();
+        jSmartTextArea.setFont(new Font("Arial", Font.ITALIC, 16));
+        jSmartTextArea.setBackground(Color.BLACK);
+        jSmartTextArea.setForeground(Color.WHITE);
+        jSmartTextArea.setEditable(false);
+        jSmartTextArea.setText(" " + ImageParser.title + " ");
+
+        jSmartTextArea.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+
+        // JDialog size and location
+        jDialog.setSize(jSmartTextArea.getPreferredSize().width + 20, jSmartTextArea.getPreferredSize().height + 20);
+//        jDialog.setLocation(1920 - jSmartTextArea.getPreferredSize().width, 1006);    // Use this on ide
+        jDialog.setLocation(1536 - jSmartTextArea.getPreferredSize().width, 806);   // Use this on package build
+
+        jDialog.add(jSmartTextArea);
+        jDialog.pack();
+        // Show title
+        jDialog.setVisible(true);
+    }
 }
--- src/ImageParser.java
+++ src/ImageParser.java
@@ -2,7 +2,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import javax.net.ssl.SSLException;
 import java.io.*;
 import java.net.NoRouteToHostException;
@@ -11,11 +10,16 @@ import java.net.UnknownHostException;
 
 public class ImageParser {
 
+    static String title = "";
+
     static void parseImage() throws InterruptedException {
         try {
-            Document doc = Jsoup.connect("https://www.bing.com/?toWww=1&redig=7D6D9A57DDD741A9A4C36F2CAB139296").get();
+            Document doc = Jsoup.connect("https://www.bing.com").get();
 
             Elements links = doc.select("head").select("link");
+
+            title = doc.getElementsByClass("title").text();
+
             String[] parsedLinks = new String[10];
             int i = 0;
             for (Element link : links){
@@ -31,7 +35,7 @@ public class ImageParser {
             InputStream in = new BufferedInputStream(url.openStream());
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             byte[] buf = new byte[1024];
-            int n = 0;
+            int n;
             while (-1!=(n=in.read(buf)))
             {
                 out.write(buf, 0, n);
